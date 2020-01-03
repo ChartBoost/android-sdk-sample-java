20efcaa4efa01b5c6515b190a3317f93242215e6
Kamil Stosik <kamil.stosik@chartboost.com>
2020-01-03 17:12:36 +0100

    MO-3917 Update sample app to work with sdk 8.0.1

--- src/main/java/com/chartboost/sdk/sample/SettingsActivity.java
+++ src/main/java/com/chartboost/sdk/sample/SettingsActivity.java
@@ -1,12 +1,10 @@
 package com.chartboost.sdk.sample;
 
-import android.content.pm.ActivityInfo;
-import android.content.res.Configuration;
 import android.os.Bundle;
 import android.preference.PreferenceActivity;
+import android.preference.PreferenceFragment;
 import android.preference.SwitchPreference;
 import android.util.Log;
-import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 
@@ -22,58 +20,45 @@ public class SettingsActivity extends PreferenceActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        addPreferencesFromResource(R.xml.preferences);
-        setUpSwitches();
+        getFragmentManager().beginTransaction().replace(android.R.id.content, new MyPreferenceFragment()).commit();
     }
 
-    private void setUpSwitches() {
-        SwitchPreference lockOrientationSwitch = (SwitchPreference) findPreference(getString(R.string.key_lock_orientation));
-        lockOrientationSwitch.setOnPreferenceChangeListener((preference, newValue) -> onlockOrientationChanged((Boolean) newValue));
-        lockOrientationSwitch.setEnabled(false);
+    public static class MyPreferenceFragment extends PreferenceFragment {
 
-        SwitchPreference setShouldRequestInterstitialsInFirstSessionSwitch = (SwitchPreference) findPreference(getString(R.string.key_request_interstitial_in_first_session));
-        setShouldRequestInterstitialsInFirstSessionSwitch.setOnPreferenceChangeListener((preference, newValue) -> onSetShouldRequestInterstitialsInFirstSessionChanged(newValue));
-
-        SwitchPreference setShouldPrefetchVideoContentSwitch = (SwitchPreference) findPreference(getString(R.string.key_enable_video_prefetch));
-        setShouldPrefetchVideoContentSwitch.setOnPreferenceChangeListener((preference, newValue) -> onSetShouldPrefetchVideoContentChanged(newValue));
-
-        SwitchPreference setAutoCacheAdsSwitch = (SwitchPreference) findPreference(getString(R.string.key_enable_autocache));
-        setAutoCacheAdsSwitch.setOnPreferenceChangeListener((preference, newValue) -> onSetAutoCacheAdsChanged(newValue));
-    }
-
-    private boolean onSetAutoCacheAdsChanged(Object newValue) {
-        Log.v(TAG, "Auto cache ads : " + newValue);
-        Chartboost.setAutoCacheAds((Boolean) newValue);
-        return true;
-    }
-
-    private boolean onSetShouldPrefetchVideoContentChanged(Object newValue) {
-        Log.v(TAG, "Prefetch set to : " + newValue);
-        Chartboost.setShouldPrefetchVideoContent((Boolean) newValue);
-        return true;
-    }
-
-    private boolean onSetShouldRequestInterstitialsInFirstSessionChanged(Object newValue) {
-        Log.v(TAG, "Request interstitials in first session : " + newValue);
-        Chartboost.setShouldRequestInterstitialsInFirstSession((Boolean) newValue);
-        return true;
-    }
-
-    private boolean onlockOrientationChanged(Boolean newValue) {
-        if (ChartboostSample.activity == null) {
-            Toast.makeText(SettingsActivity.this, "Unable to lock orientation, Debug Activity not initiated", Toast.LENGTH_SHORT).show();
-            return false;
+        @Override
+        public void onCreate(final Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            addPreferencesFromResource(R.xml.preferences);
+            setUpSwitches();
         }
-        if (newValue) {
-            int currentOrientation = getResources().getConfiguration().orientation;
-            if (currentOrientation == Configuration.ORIENTATION_LANDSCAPE) {
-                ChartboostSample.activity.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_SENSOR_LANDSCAPE);
-            } else {
-                ChartboostSample.activity.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_SENSOR_PORTRAIT);
-            }
-        } else {
-            ChartboostSample.activity.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_UNSPECIFIED);
+
+        private void setUpSwitches() {
+            SwitchPreference setShouldRequestInterstitialsInFirstSessionSwitch = (SwitchPreference) findPreference(getString(R.string.key_request_interstitial_in_first_session));
+            setShouldRequestInterstitialsInFirstSessionSwitch.setOnPreferenceChangeListener((preference, newValue) -> onSetShouldRequestInterstitialsInFirstSessionChanged(newValue));
+
+            SwitchPreference setShouldPrefetchVideoContentSwitch = (SwitchPreference) findPreference(getString(R.string.key_enable_video_prefetch));
+            setShouldPrefetchVideoContentSwitch.setOnPreferenceChangeListener((preference, newValue) -> onSetShouldPrefetchVideoContentChanged(newValue));
+
+            SwitchPreference setAutoCacheAdsSwitch = (SwitchPreference) findPreference(getString(R.string.key_enable_autocache));
+            setAutoCacheAdsSwitch.setOnPreferenceChangeListener((preference, newValue) -> onSetAutoCacheAdsChanged(newValue));
         }
-        return true;
+
+        private boolean onSetAutoCacheAdsChanged(Object newValue) {
+            Log.v(TAG, "Auto cache ads : " + newValue);
+            Chartboost.setAutoCacheAds((Boolean) newValue);
+            return true;
+        }
+
+        private boolean onSetShouldPrefetchVideoContentChanged(Object newValue) {
+            Log.v(TAG, "Prefetch set to : " + newValue);
+            Chartboost.setShouldPrefetchVideoContent((Boolean) newValue);
+            return true;
+        }
+
+        private boolean onSetShouldRequestInterstitialsInFirstSessionChanged(Object newValue) {
+            Log.v(TAG, "Request interstitials in first session : " + newValue);
+            Chartboost.setShouldRequestInterstitialsInFirstSession((Boolean) newValue);
+            return true;
+        }
     }
 }
